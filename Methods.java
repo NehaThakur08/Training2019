159c89e2d85ac840ccf50ff5985b71db172c8f04
NehaThakur08 <46674543+NehaThakur08@users.noreply.github.com>
2019-01-23 22:01:35 +0530

    Update Methods.java

--- Methods.java
+++ Methods.java
@@ -14,9 +14,9 @@ void multiplynum2(int x,int y){ // non static method
 		int b=4;
 		int c=a*b;
 		System.out.println("The value of c:"+c);
-		Methods.multiplynum(20,30);
+		Methods.multiplynum(20,30); //execution of static method
 		System.out.println("");
-		Methods multiref=new Methods();
+		Methods multiref=new Methods(); //execution of non static method
 		multiref.multiplynum(20,3);
 		multiref.multiplynum(10,10);
 	}
