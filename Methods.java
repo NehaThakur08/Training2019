9d1869e5de6aa0ed5d99fb36c89c8300ce11954d
NehaThakur08 <46674543+NehaThakur08@users.noreply.github.com>
2019-01-23 22:00:24 +0530

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,24 @@
+
+public class Methods {
+static void multiplynum(int x,int y){ //static method
+	 int z= x * y;
+	 System.out.println("Result of multiplication is:"+z);
+ }
+void multiplynum2(int x,int y){ // non static method
+	 int z= x * y;
+	 System.out.println("Result of multiplication is:"+z);
+}
+ 
+	public static void main(String[] args) {
+		int a=10;
+		int b=4;
+		int c=a*b;
+		System.out.println("The value of c:"+c);
+		Methods.multiplynum(20,30);
+		System.out.println("");
+		Methods multiref=new Methods();
+		multiref.multiplynum(20,3);
+		multiref.multiplynum(10,10);
+	}
+
+}
